e941a902bbbbef3b0f40fcdb7f0064572d3eea18
JoakoLms <joakolms428@gmai.com>
2024-09-28 18:58:35 -0300

    hice un main para probar que el sistema funciona

---
+++ ISFPP2024JN/src/aplicacion/TestCompilacion.java
@@ -0,0 +1,10 @@
+package aplicacion;
+
+public class TestCompilacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("El proyecto anda bien");
+	}
+
+}
