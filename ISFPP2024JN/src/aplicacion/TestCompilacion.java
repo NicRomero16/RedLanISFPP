3da21a9ffee9338a81624a5d3911d1cab161fc89
JoakoLms <joakolms428@gmai.com>
2024-10-09 21:50:32 -0300

    Graph

--- ISFPP2024JN/src/aplicacion/TestCompilacion.java
+++ ISFPP2024JN/src/aplicacion/TestCompilacion.java
@@ -1,15 +1,20 @@
 package aplicacion;
 
 import java.io.IOException;
+
 import java.util.List;
 import java.util.TreeMap;
 
+import controlador.Coordinador;
+import negocio.Calculo;
+import dao.*;
 import dao.secuencial.*;
 import modelo.*;
 
 public class TestCompilacion {
 
 	public static void main(String[] args) throws IOException {
+
 		TipoEquipoSecuencialDAO dao = new TipoEquipoSecuencialDAO();
 		TipoCableSecuencialDAO dao1 = new TipoCableSecuencialDAO();
 		TipoPuertoSecuencialDAO dao2 = new TipoPuertoSecuencialDAO();
@@ -17,23 +22,52 @@ public class TestCompilacion {
 		ConexionSecuencialDAO dao4 = new ConexionSecuencialDAO();
 		EquipoSecuencialDAO dao5 = new EquipoSecuencialDAO();
 
-		TreeMap<String, TipoEquipo> tipoEquipo = dao.buscarTodos();
-		System.out.println(tipoEquipo);
+		TreeMap<String, TipoEquipo> tipoEquipos = dao.buscarTodos();
+		TreeMap<String, TipoCable> tipoCables = dao1.buscarTodos();
+		TreeMap<String, TipoPuerto> tipoPuertos = dao2.buscarTodos();
+		TreeMap<String, Ubicacion> ubicaciones = dao3.buscarTodos();
+		List<Conexion> conexiones = dao4.buscarTodos();
+		TreeMap<String, Equipo> equipos = dao5.buscarTodos();
+
+		System.out.println("Equipos cargados:");
+		for (Equipo equipo : equipos.values()) {
+			System.out.println(equipo);
+		}
+		
 		System.out.println();
-		TreeMap<String, TipoCable> tipoCable = dao1.buscarTodos();
-		System.out.println(tipoCable);
+
+		System.out.println("Conexiones cargadas:");
+		for (Conexion conexion : conexiones) {
+			System.out.println(conexion);
+		}
+		
 		System.out.println();
-		TreeMap<String, TipoPuerto> tipoPuerto = dao2.buscarTodos();
-		System.out.println(tipoPuerto);
+
+		System.out.println("Tipos de equipos cargados:");
+		for (TipoEquipo tipoEquipo : tipoEquipos.values()) {
+			System.out.println(tipoEquipo);
+		}
+		
 		System.out.println();
-		TreeMap<String, Ubicacion> ubicacion = dao3.buscarTodos();
-		System.out.println(ubicacion);
+		
+		System.out.println("Tipos de cables cargados:");
+		for (TipoCable cables : tipoCables.values()) {
+			System.out.println(cables);
+		}
+
 		System.out.println();
-		List<Conexion> conexion = dao4.buscarTodos();
-		System.out.println(conexion);
+		
+		System.out.println("Tipos de puertos cargados:");
+		for (TipoPuerto puertos : tipoPuertos.values()) {
+			System.out.println(puertos);
+		}
+
 		System.out.println();
-		TreeMap<String, Equipo> equipo = dao5.buscarTodos();
-		System.out.println(equipo);
+		
+		System.out.println("Ubicaciones cargadas:");
+		for (Ubicacion ubi : ubicaciones.values()) {
+			System.out.println(ubi);
+		}
 	}
 
 }
--- ISFPP2024JN/src/negocio/Calculo.java
+++ ISFPP2024JN/src/negocio/Calculo.java
@@ -1,7 +1,6 @@
 package negocio;
 
-import org.jgrapht.Graph;
-import org.jgrapht.graph.SimpleGraph;
+import org.jgrapht.graph.*;
 
 import controlador.Coordinador;
 import modelo.Equipo;
@@ -13,22 +12,42 @@ import java.util.TreeMap;
 public class Calculo {
 
 	private Coordinador coordinador;
-	private Graph<Equipo, Conexion> red;
-	private TreeMap<String, Equipo> equiposMap;
 
-	public Calculo(List<Equipo> equipos, List<Conexion> conexiones) {
+	private SimpleGraph<Equipo, Conexion> red;
+	private TreeMap<String, Equipo> vertices;
+
+	public Calculo() {
+	}
+
+	public void cargarEquipos(TreeMap<String, Equipo> eq, List<Conexion> conexiones) {
 
 		red = new SimpleGraph<>(Conexion.class);
-		equiposMap = new TreeMap<String, Equipo>();
 
-		for (Equipo e : equipos) {
-			equiposMap.put(e.getCodigo(), e);
-			red.addVertex(e);
+		// Cargar equipos
+		for (Equipo equipo : eq.values()) {
+			red.addVertex(equipo);
+			vertices.put(equipo.getCodigo(), equipo);
 		}
-		for (Conexion c : conexiones) {
-			Equipo equipo1 = equiposMap.get(c.getEquipo1().getCodigo());
-			Equipo equipo2 = equiposMap.get(c.getEquipo2().getCodigo());
-			red.addEdge(equipo1, equipo2, c);
+		// Cargar conexiones
+		for (Conexion conexion : conexiones) {
+			Equipo equipo1 = conexion.getEquipo1();
+			Equipo equipo2 = conexion.getEquipo2();
+
+			if (red.containsVertex(equipo1) && red.containsVertex(equipo2))
+				// Agregar la arista entre equipo1 y equipo2
+				red.addEdge(equipo1, equipo2, conexion);
+			else
+				System.out.println("Error: Uno o ambos equipos no están en el grafo.");
+
 		}
 	}
+
+	public TreeMap<String, Equipo> getVertices() {
+		return vertices;
+	}
+
+	public SimpleGraph<Equipo, Conexion> getRed() {
+		return red;
+	}
+
 }
